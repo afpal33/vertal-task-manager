9f3604497469f98c32387ba225b31169e197c4c7
Andy Fabrizio Palenque Barriga <128990027+afpal33@users.noreply.github.com>
2026-09-19 02:06:34 +0000

    fix: new normal user should now be able to join the team

--- backend/src/main/java/com/fabrizioroot/vertal/repository/DispositivoRepository.java
+++ backend/src/main/java/com/fabrizioroot/vertal/repository/DispositivoRepository.java
@@ -1,8 +1,10 @@
 package com.fabrizioroot.vertal.repository;
 import com.fabrizioroot.vertal.model.Dispositivo;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 public interface DispositivoRepository extends JpaRepository<Dispositivo, Long> {
+    @EntityGraph(attributePaths = "usuario")
     Optional<Dispositivo> findByIdentificadorDispositivoAndActivoTrue(String identificadorDispositivo);
     boolean existsByUsuarioIdAndActivoTrue(Long usuarioId);
 }
